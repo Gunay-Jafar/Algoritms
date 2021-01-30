19c88c63770e43a725da0b8eef60e9da96098598
Gunay-Jafar <cafarligunay06@gmail.com>
2021-01-30 16:30:23 +0400

    Task 4716

---
+++ src/eOlimp/Task4716.java
@@ -0,0 +1,14 @@
+package eOlimp;
+
+import java.util.Scanner;
+
+//100
+public class Task4716 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int k=scanner.nextInt();
+        int apple=k/n;
+        System.out.println(apple);
+    }
+}
