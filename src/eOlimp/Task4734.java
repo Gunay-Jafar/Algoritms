5a41261bf71d2a3209c64d2a9428b9cfe4e75d0a
Gunay-Jafar <cafarligunay06@gmail.com>
2021-02-18 23:51:15 +0400

    Added Task4734

--- src/eOlimp/Task4734.java
+++ src/eOlimp/Task4734.java
@@ -1,4 +1,68 @@
 package eOlimp;
 
+import java.util.Scanner;
+
+//100
 public class Task4734 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();
+        int num1 = num.charAt(0) - '0';
+
+        if (num.length() == 1) {
+            if (num1 % 3 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+            if (num1 % 6 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+            if (num1 % 9 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+        }
+
+        if (num.length() == 2) {
+            int num2 = num.charAt(1) - '0';
+            if ((num1 * 10 + num2) % 3 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+            if ((num1 * 10 + num2) % 6 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+            if ((num1 * 10 + num2) % 9 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+        }
+
+        if (num.length() >= 3) {
+            int sum = 0;
+            int lastOne = num.charAt(num.length() - 1) - '0';
+            for (int i = 0; i < num.length(); i++) {
+                int nums = num.charAt(i) - '0';
+                sum += nums;
+            }
+
+            if (sum % 3 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+
+            if ((sum % 3 == 0) && (lastOne % 2 == 0)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+
+            if (sum % 9 == 0) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+        }
+    }
 }
