eae4273a4f4ec74e1d85375c9303622ea4fbcbd2
Gunay-Jafar <cafarligunay06@gmail.com>
2021-02-19 00:09:04 +0400

    Added Task 4736

--- src/eOlimp/Task4736.java
+++ src/eOlimp/Task4736.java
@@ -2,14 +2,27 @@ package eOlimp;
 
 import java.util.Scanner;
 
+//100
 public class Task4736 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        long a=scanner.nextInt();
-        if (a%11==0){
-            System.out.println("Yes");
-        }else {
-            System.out.println("No");
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();
+        int sumTek = 0;
+        int sumCut = 0;
+
+        for (int i = 0; i < num.length(); i = i + 2) {
+            int numCut = num.charAt(i) - '0';
+            sumCut += numCut;
         }
+
+        for (int i = 1; i < num.length(); i = i + 2) {
+            int numTek = num.charAt(i) - '0';
+            sumTek += numTek;
+        }
+
+        if ((sumCut - sumTek) % 11 == 0) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
     }
 }
