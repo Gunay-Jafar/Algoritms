c53b6c6028c34e7337921e4bb138a0567b9328da
Gunay-Jafar <cafarligunay06@gmail.com>
2021-02-18 23:59:24 +0400

    Task 4735

--- src/eOlimp/Task4735.java
+++ src/eOlimp/Task4735.java
@@ -2,9 +2,20 @@ package eOlimp;
 
 import java.util.Scanner;
 
+//100
 public class Task4735 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.next();
+        int last = num.charAt(num.length() - 1) - '0';
+        if ((last == 0) || (last == 5)) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
 
+        if (last == 0) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
     }
 }
